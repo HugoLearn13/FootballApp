ab5ad960368432635bb91241ec50021794f8db9f
HugoLearn13 <szasz.hunor13@yahoo.com>
2020-02-10 22:57:06 +0200

    Project initialized

--- src/App.js
+++ src/App.js
@@ -1,26 +1,24 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import Card from './Card';
+import {footballers} from './footballers';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+
+	constructor(){
+		super();
+	}
+
+	render() {
+		return (
+			<div className ="tc">
+				<h1>"Top footballers"</h1>
+				<Card id={footballers[0].id} name={footballers[0].name} position={footballers[0].position} />
+				<Card id={footballers[1].id} name={footballers[1].name} position={footballers[1].position} />
+				<Card id={footballers[2].id} name={footballers[2].name} position={footballers[2].position} />
+				<Card id={footballers[3].id} name={footballers[3].name} position={footballers[3].position} />
+				<Card id={footballers[4].id} name={footballers[4].name} position={footballers[4].position} />
+			</div> 
+		)
+	}
 }
-
 export default App;
---
+++ src/footballers.js
@@ -0,0 +1,32 @@
+export const footballers = [
+	{
+		id: 1,
+		name: 'Lionel Messi',
+		position: 'CF'
+	},
+
+	{
+		id: 2,
+		name: 'Manuel Neuer',
+		position: 'GK'
+	},
+
+	{
+		id: 3,
+		name: 'Mesut Ozil',
+		position: 'CM'
+	},
+
+	{
+		id: 4,
+		name: 'Alvaro Morata',
+		position: 'CF'
+	},
+
+	{
+		id: 5,
+		name: 'Cristiano Ronaldo',
+		position: 'CF'
+	}
+
+]
